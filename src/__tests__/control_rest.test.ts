9e6b240272c4768c4abe5d97221f65b7839c6c33
Richard Cartwright <spark@streampunk.media>
2019-06-20 18:57:41 +0100

    chore: added unhappy path tests to control-level HTTP tests

--- src/__tests__/control_rest.test.ts
+++ src/__tests__/control_rest.test.ts
@@ -40,6 +40,16 @@ describe('Control-level REST API tests', () => {
 		})
 	})
 
+	test('START playing using server name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/START').then(JSON.parse))
+		.resolves.toMatchObject({
+			type: 'TriggerResult',
+			serverID: 1100,
+			portName: 'Port 1',
+			success: true
+		})
+	})
+
 	test('STOP playing using server number', async () => {
 		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/trigger/STOP?offset=123').then(JSON.parse))
 		.resolves.toMatchObject({
@@ -51,7 +61,18 @@ describe('Control-level REST API tests', () => {
 		})
 	})
 
-	test('JUMP now', async () => {
+	test('STOP playing using server name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/STOP?offset=123').then(JSON.parse))
+		.resolves.toMatchObject({
+			type: 'TriggerResult',
+			serverID: 1100,
+			portName: 'Port 1',
+			success: true,
+			offset: 123
+		})
+	})
+
+	test('JUMP now using server number', async () => {
 		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/trigger/JUMP').then(JSON.parse))
 		.resolves.toMatchObject({
 			type: 'TriggerResult',
@@ -61,7 +82,59 @@ describe('Control-level REST API tests', () => {
 		})
 	})
 
-	test('Hard jump', async () => {
+	test('JUMP now using server name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/JUMP').then(JSON.parse))
+		.resolves.toMatchObject({
+			type: 'TriggerResult',
+			serverID: 1100,
+			portName: 'Port 1',
+			success: true
+		})
+	})
+
+	test('Attempt to trigger with an unknown trigger type', async () => {
+		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/trigger/JUMPY'))
+		.rejects.toThrow('Trigger must have path parameter \'START\', \'STOP\' or \'JUMP\'')
+		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/trigger/JUMPY'))
+		.rejects.toThrow('400')
+	})
+
+	test('Attempt to trigger with a bad server number', async () => {
+		await expect(request.post('http://localhost:3000/default/server/1234/port/Port 1/trigger/START'))
+		.rejects.toThrow('Could not find a server with identifier \'1234\'')
+		await expect(request.post('http://localhost:3000/default/server/1234/port/Port 1/trigger/START'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt to trigger with a bad server name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Smelly 1100/port/Port 1/trigger/START'))
+		.rejects.toThrow('Could not find a server with identifier \'Smelly 1100\'')
+		await expect(request.post('http://localhost:3000/default/server/Smelly 1100/port/Port 1/trigger/START'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt to trigger with a bad port name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/portal/trigger/START'))
+		.rejects.toThrow('Could not find a port called \'portal\' on server \'Server 1100\'')
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/portal/trigger/START'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt to trigger with a non-integer offset', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/START?offset=wtf'))
+		.rejects.toThrow('Optional offset parameter must be a non-negative integer.')
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/START?offset=wtf'))
+		.rejects.toThrow('400')
+	})
+
+	test('Attempt to trigger with a negative offset', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/START?offset=-1'))
+		.rejects.toThrow('Optional offset parameter must be a non-negative integer.')
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/trigger/START?offset=-1'))
+		.rejects.toThrow('400')
+	})
+
+	test('Hard jump with server number', async () => {
 		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=101').then(JSON.parse))
 		.resolves.toMatchObject({
 			type: 'HardJumpResult',
@@ -72,7 +145,53 @@ describe('Control-level REST API tests', () => {
 		})
 	})
 
-	test('Soft jump', async () => {
+	test('Hard jump with server name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Server 1100/port/Port 1/jump?offset=101').then(JSON.parse))
+		.resolves.toMatchObject({
+			type: 'HardJumpResult',
+			serverID: 1100,
+			portName: 'Port 1',
+			success: true,
+			offset: 101
+		})
+	})
+
+	test('Attempt hard jump with bad server ident', async () => {
+		await expect(request.post('http://localhost:3000/default/server/1987/port/Port 1/jump?offset=123'))
+		.rejects.toThrow('Could not find a server with identifier \'1987\'')
+		await expect(request.post('http://localhost:3000/default/server/1987/port/Port 1/jump?offset=123'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt hard jump with bad server name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/Smelly 1100/port/Port 1/jump?offset=123'))
+		.rejects.toThrow('Could not find a server with identifier \'Smelly 1100\'')
+		await expect(request.post('http://localhost:3000/default/server/Smelly 1100/port/Port 1/jump?offset=123'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt hard jump with bad port name', async () => {
+		await expect(request.post('http://localhost:3000/default/server/1100/port/portal/jump?offset=123'))
+		.rejects.toThrow('Could not find a port called \'portal\' on server \'Server 1100\'')
+		await expect(request.post('http://localhost:3000/default/server/1100/port/portal/jump?offset=123'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt hard jump with non-number offset', async () => {
+		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=wtf'))
+		.rejects.toThrow('Optional offset parameter must be a non-negative integer')
+		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=wtf'))
+		.rejects.toThrow('400')
+	})
+
+	test('Attempt hard jump with negative offset', async () => {
+		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=-1'))
+		.rejects.toThrow('Optional offset parameter must be a non-negative integer')
+		await expect(request.post('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=-1'))
+		.rejects.toThrow('400')
+	})
+
+	test('Soft jump with server number', async () => {
 		await expect(request.put('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=101').then(JSON.parse))
 		.resolves.toMatchObject({
 			type: 'TriggeredJumpResult',
@@ -80,6 +199,51 @@ describe('Control-level REST API tests', () => {
 			portName: 'Port 1',
 			offset: 101
 		})
+	})
+
+	test('Soft jump with server name', async () => {
+		await expect(request.put('http://localhost:3000/default/server/Server 1100/port/Port 1/jump?offset=101').then(JSON.parse))
+		.resolves.toMatchObject({
+			type: 'TriggeredJumpResult',
+			serverID: 1100,
+			portName: 'Port 1',
+			offset: 101
+		})
+	})
+
+	test('Attempt soft jump with bad server ident', async () => {
+		await expect(request.put('http://localhost:3000/default/server/1123/port/Port 1/jump?offset=321'))
+		.rejects.toThrow('Could not find a server with identifier \'1123\'')
+		await expect(request.put('http://localhost:3000/default/server/1123/port/Port 1/jump?offset=321'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt soft jump with bad server name', async () => {
+		await expect(request.put('http://localhost:3000/default/server/Smelly 1100/port/Port 1/jump?offset=321'))
+		.rejects.toThrow('Could not find a server with identifier \'Smelly 1100\'')
+		await expect(request.put('http://localhost:3000/default/server/Smelly 1100/port/Port 1/jump?offset=321'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt soft jump with bad port name', async () => {
+		await expect(request.put('http://localhost:3000/default/server/1100/port/portal/jump?offset=321'))
+		.rejects.toThrow('Could not find a port called \'portal\' on server \'Server 1100\'')
+		await expect(request.put('http://localhost:3000/default/server/1100/port/portal/jump?offset=321'))
+		.rejects.toThrow('404')
+	})
+
+	test('Attempt soft jump with non-number offset', async () => {
+		await expect(request.put('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=wtf'))
+		.rejects.toThrow('Optional offset parameter must be a non-negative integer')
+		await expect(request.put('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=wtf'))
+		.rejects.toThrow('400')
+	})
+
+	test('Attempt soft jump with negative offset', async () => {
+		await expect(request.put('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=-1'))
+		.rejects.toThrow('Optional offset parameter must be a non-negative integer')
+		await expect(request.put('http://localhost:3000/default/server/1100/port/Port 1/jump?offset=-1'))
+		.rejects.toThrow('400')
 	})
 
 	afterAll(async () => {
